af0461fe16abf08bf9d9732ca8a01cd23f48834a
Vitor <145723026+Vitor191@users.noreply.github.com>
2024-01-12 00:58:35 -0300

    alteracao

--- Exercicios/ex020.py
+++ Exercicios/ex020.py
@@ -5,7 +5,6 @@ aluno2 = input("Digite o nome do segundo aluno")
 aluno3 = input("Digite o nome do terceiro aluno")
 aluno4 = input("Digite o nome do quarto aluno")
 
-
 individuos1 = [aluno1, aluno2, aluno3, aluno4]
 individuos2 = individuos1.copy()
 random.shuffle(individuos1)
