dd3d7bcc7412d8f45e3724f3e22c956d7f06e310
Vitor <145723026+Vitor191@users.noreply.github.com>
2024-01-12 01:01:37 -0300

    alteracao

--- Exercicios/ex020.py
+++ Exercicios/ex020.py
@@ -1,13 +1,13 @@
-import random
+from random import shuffle
 
 aluno1 = input("Digite o nome do primeiro aluno")
 aluno2 = input("Digite o nome do segundo aluno")
 aluno3 = input("Digite o nome do terceiro aluno")
 aluno4 = input("Digite o nome do quarto aluno")
 
-individuos1 = [aluno1, aluno2, aluno3, aluno4]
-individuos2 = individuos1.copy()
-random.shuffle(individuos1)
+individuos = [aluno1, aluno2, aluno3, aluno4]
+individuos1 = individuos.copy()
+shuffle(individuos1)
 
-print(f"a lista original e:{individuos2}")
+print(f"a lista original e:{individuos}")
 print(f"A lista randomizada e:{individuos1}")
