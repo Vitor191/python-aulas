1e65fc2240e420464c47cd4fff1042d4257aa234
Vitor <145723026+Vitor191@users.noreply.github.com>
2024-01-19 00:50:45 -0300

    novos ex

--- Exercicios/ex043.py
+++ Exercicios/ex043.py
@@ -5,11 +5,11 @@ imc = peso / (altura * altura)
 
 if imc < 18.5:
     print(f"Seu IMC e {imc} e voce esta abaixo do peso")
-elif imc >= 18.5 and imc < 25:
+elif 18.5 <= imc < 25:
     print(f"Seu IMC e {imc} e voce esta no peso ideal")
-elif imc >= 25 and imc < 30:
+elif 25 <= imc < 30:
     print(f"Seu IMC e {imc} e voce esta com sobrepeso")
-elif imc >= 30 and imc < 40:
+elif 30 <= imc < 40:
     print(f"Seu IMC e {imc} e voce esta com obesidade")
 elif imc >= 40:
     print(f"Seu IMC e {imc} e voce esta com obesidade morbida")
