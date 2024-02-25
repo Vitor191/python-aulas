f85646144636534ea68b28efa1a846b4deff6ef5
Vitor <145723026+Vitor191@users.noreply.github.com>
2024-02-25 01:04:24 -0300

    novos ex

--- Exercicios/ex090.py
+++ Exercicios/ex090.py
@@ -1,10 +1,11 @@
 aluno = {}
 aluno["nome"] = str(input("Nome:"))
 aluno["media"] = float(input(f"Media de {aluno["nome"]}:"))
-if aluno["media"] >= 6:
+if aluno["media"] >= 7:
     aluno['situacao'] = 'Aprovado'
+elif 5 <= aluno['media'] < 7:
+    aluno['situacao'] = 'Recuperacao'
 else:
     aluno['situacao'] = 'Reprovado'
-print(f"Nome e igual a {aluno["nome"]}")
-print(f'A media e igual a {aluno["media"]}')
-print(f'A situacao do aluno e igual a {aluno['situacao']}')
+for k, v in aluno.items():
+    print(f' - {k} e igual a {v}')
--- Exercicios/ex091.py
+++ Exercicios/ex091.py
@@ -1,22 +1,46 @@
-from random import randint
+# from random import randint
+# from time import sleep
+#
+# dic = {}
+# jogador = []
+# ordem = []
+# print("Valores sorteados")
+# for c in range(4):
+#     valor = randint(1, 6)
+#     dic = {"dado": valor}
+#     print(f"O jogador{c+1} tirou {valor}")
+#     jogador.append(dic)
+#     sleep(1)
+# print("=-=" * 10)
+# for a in jogador:
+#     ordem.append(a["dado"])
+#     ordem.sort(reverse=True)
+# jogador_copia = jogador.copy()
+# for a, b in enumerate(ordem):
+#     for c, d in enumerate(jogador_copia):
+#         if d["dado"] == b:
+#             print(f"O {a+1} lugar ficou com o jogador{c+1} que tirou {b}")
+#             d["dado"] = 0
+#             sleep(1)
+#             break
 
-dic = {}
-jogador = []
-ordem = []
+from random import randint
+from time import sleep
+from operator import itemgetter
+
+jogo = {
+    "jogador1": randint(1, 6),
+    "jogador2": randint(1, 6),
+    "jogador3": randint(1, 6),
+    "jogador4": randint(1, 6),
+}
+ranking = list()
 print("Valores sorteados")
-for c in range(4):
-    valor = randint(1, 6)
-    dic = {"dado": valor}
-    print(f"O jogador{c+1} tirou {valor}")
-    jogador.append(dic)
-print("=-=" * 10)
-for a in jogador:
-    ordem.append(a["dado"])
-    ordem.sort(reverse=True)
-jogador_copia = jogador.copy()
-for a, b in enumerate(ordem):
-    for c, d in enumerate(jogador_copia):
-        if d["dado"] == b:
-            print(f"O {a+1} lugar ficou com o jogador{c+1} que tirou {b}")
-            d["dado"] = 0
-            break
+for k, v in jogo.items():
+    print(f"{k} tirou {v} no dado")
+    sleep(1)
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+print("=-=" * 15)
+for k, v in enumerate(ranking):
+    print(f"{k+1} lugar: {v[0]} com {v[1]}.")
+    sleep(1)
